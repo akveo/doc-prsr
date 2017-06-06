83724daa009fae9c23acd1e11b4d175fb6c889de
Yauhen <penkin.evgeniy@gmail.com>
2017-06-06 19:29:08 +0300

    build(project): commander.js lib added

--- src/index.ts
+++ src/index.ts
@@ -6,87 +6,10 @@ import * as fs from 'fs';
 fs.readFile('./input-examples/docJSInput.json', function (err: any, data: any) {
     let p1 = new DocJSParser();
     // let newdoc = p1.parse(JSON.parse(data));
-    // console.log(p1.parse(JSON.parse(data)));
+    console.log(p1.parse(JSON.parse(data)));
     // const outputObj = JSON.stringify(p1.parse(JSON.parse(data)));
     // console.log(outputObj);
 });
-
-const docJSParser = new DocJSParser();
-
-   const input = {
-     properties: [
-      {
-        title: 'property'
-        kind: 'prop',
-        platform: 'ios',
-        hello: 'world',
-        me: 1,
-        isStatic: true,
-        type: 'some-type',
-        required: false,
-        name: 'awesome property',
-        isKing: false,
-        description: { type: 'some desc'},
-        shortDescription: '123'
-      },
-      {
-        code: 'void main(){ cout>>asdasd; }',
-        description: 'some description 2',
-        prop: 22,
-        bebe: true,
-        some: null
-      },
-      {
-        title: 'property'
-        kind: 'prop',
-        platform: 'ios',
-        hello: 'world',
-        me: 1,
-        isStatic: true,
-        type: 'some-type',
-        required: false,
-        name: 'awesome property',
-        isKing: false,
-        description: { type: 'some desc'},
-        shortDescription: '123'
-      }
-    ]
-  };
-    const output = [
-      new Prop({
-        title: 'property'
-        kind: 'prop',
-        platform: 'ios',
-        hello: 'world',
-        me: 1,
-        isStatic: true,
-        type: 'some-type',
-        required: false,
-        name: 'awesome property',
-        isKing: false,
-        description: { type: 'some desc'},
-        shortDescription: '123'
-      }),
-      null,
-      new Prop({
-        title: 'property'
-        kind: 'prop',
-        platform: 'ios',
-        hello: 'world',
-        me: 1,
-        isStatic: true,
-        type: 'some-type',
-        required: false,
-        name: 'awesome property',
-        isKing: false,
-        description: { type: 'some desc'},
-        shortDescription: '123'
-      })
-  ];
-
-    console.log(docJSParser.getProps(input).toString() === output.toString());
-    // console.log('---------------------');
-    // console.log(output);
 
 
 
