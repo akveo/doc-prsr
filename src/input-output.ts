44965be5e543727f6284fef0c33c11cad241109d
Yauhen <penkin.evgeniy@gmail.com>
2017-06-13 17:50:55 +0300

    fix(InputOutput): pathes refactoring

--- src/input-output.ts
+++ src/input-output.ts
@@ -1,30 +1,37 @@
-import { DocJsParser } from './doc-js.parser/doc-js.parser';
+import {DocJsParser} from './doc-js.parser/doc-js.parser';
 import * as fs from 'fs';
+import * as Path from 'path';
 const program = require('commander');
 
 export class InputOutput {
   inputStr: any = '';
   outputStr: any = '';
 
-  setInputOutputPathes(): void {
+  setInputOutputPathes(): boolean {
     program
       .version('0.0.1')
       .option('-i, --input <value>', 'Path to input file:')
       .option('-o, --output <value>', 'Path to output file: ')
       .parse(process.argv);
 
-    this.inputStr = program['input'];
-    this.outputStr = program['output'];
+    if (program['input'] && program['output']) {
+      this.inputStr = Path.resolve(program['input']);
+      this.outputStr = Path.resolve(program['output']);
+      return true;
+    } else {
+      console.log('U don\'t enter inp and/or out str');
+      return false;
+    }
   }
 
   createFile(): void {
-    this.setInputOutputPathes();
-
-    fs.readFile(process.cwd() + this.inputStr, (err: any, data: any) => {
-      const newdoc = new DocJsParser().parse(JSON.parse(data));
-      const outputObj: any = JSON.stringify(newdoc, null, 2);
-      fs.writeFile(process.cwd() + this.outputStr, outputObj);
-    });
+    if (this.setInputOutputPathes()) {
+      fs.readFile(this.inputStr, (err: any, data: any) => {
+        const newdoc = new DocJsParser().parse(JSON.parse(data));
+        const outputObj: any = JSON.stringify(newdoc, null, 2);
+        fs.writeFile(this.outputStr, outputObj);
+      });
+    }
   }
 
 }
