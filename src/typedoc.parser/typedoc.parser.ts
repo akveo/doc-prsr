b70a452d48e45232ec451d9f37c468918087801d
Yauhen <penkin.evgeniy@gmail.com>
2017-06-14 12:34:16 +0300

    feat(TypedocParser): "findAllClasses" method added

--- src/typedoc.parser/typedoc.parser.ts
+++ src/typedoc.parser/typedoc.parser.ts
@@ -11,6 +11,25 @@ import {
 } from '../model';
 
 export class TypedocParser {
+  protected json: any;
+  protected classes: any[] = [];
+
+  saveJSON(json: any) {
+    this.json = json;
+  }
+
+  findAllClasses(obj: any) {
+    if (obj && obj[CommonOptions.children]) {
+      obj[CommonOptions.children].forEach((item: any) => {
+        if (item[CommonOptions.primKind] === 'Class' || item[CommonOptions.primKind] === 'Interface') {
+          this.classes.push(item)
+        } else {
+          this.findAllClasses(item);
+        }
+      })
+    }
+  }
+
 
 
 
