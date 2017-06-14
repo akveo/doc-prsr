f08b42a86fe9924edbb49ee075c4b8c791ff1657
Yauhen <penkin.evgeniy@gmail.com>
2017-06-14 14:17:17 +0300

    feat(GetProperties): "getProps" method added

--- src/typedoc.parser/getters/getProperties.ts
+++ src/typedoc.parser/getters/getProperties.ts
@@ -2,5 +2,25 @@ import { Prop } from '../../model';
 import {CommonOptions} from '../typedoc.parser.options';
 
 export class GetProperties {
-  
+  getProps(obj: any) {
+    if (obj && obj[CommonOptions.children]) {
+      obj[CommonOptions.children].map((item: any) => {
+        if (item[CommonOptions.decorators]) {
+          if(item[CommonOptions.primKind] === 'Property' && item[CommonOptions.decorators][CommonOptions.name] === 'Input') {
+            return this.parseInput(item);
+          } else if (item[CommonOptions.primKind] === 'Property' && item[CommonOptions.decorators][CommonOptions.name] === 'Output') {
+            return this.parseOutput(item);
+          }
+        } else {
+          if (item[CommonOptions.primKind] === 'Constructor') {
+            return this.parseConstructor(item);
+          } else if (item[CommonOptions.primKind] === 'Property') {
+            return this.parseProperty(item);
+          }
+        }
+      })
+    }
+  }
+
+
 }
