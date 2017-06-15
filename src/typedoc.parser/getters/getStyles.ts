0cb067474cd7f0738fd5738db814712adbd98cb3
Yauhen <penkin.evgeniy@gmail.com>
2017-06-15 14:05:21 +0300

    feat(GetStyles): GetStyles shell added

---
+++ src/typedoc.parser/getters/getStyles.ts
@@ -0,0 +1,23 @@
+import {
+  Style
+} from '../../model';
+import {CommonOptions} from '../typedoc.parser.options';
+
+export  class GetStyles {
+
+  getStyles(obj: any) {
+
+  }
+
+  parserStyle(obj: any) {
+
+  }
+
+  getStylesOfStyle(obj: any) {
+
+  }
+
+  getShortDescription(obj: any) {
+
+  }
+}
