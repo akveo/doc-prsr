9a514f757ff170186186965572b10d26c0d61305
tibing <parapupum@gmail.com>
2017-06-27 15:39:50 +0300

    fix(package): prepare to publish

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const { createConfig, entryPoint, setOutput } = require('@webpack-blocks/webpack2');
-const typescript = require('@webpack-blocks/typescript');
-
-module.exports = createConfig([
-  entryPoint('./src/index.ts'),
-  setOutput('./build/bundle.js'),
-  typescript(),
-  () => ({target: 'node'}),
-]);
