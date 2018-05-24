6436fd1acaca0221e8fef9ab4863e9545a75e3e0
Nikita Poltoratsky <parapupum@gmail.com>
2018-05-24 11:32:37 +0300

    feat(examples): add capability insert names (#14)

--- src/model/class/overview.ts
+++ src/model/class/overview.ts
@@ -1,6 +1,14 @@
-import { InlineExample } from './example';
+import { Example } from './example';
 
 export class OverviewNode {
-  type: 'text' | 'live-example' | 'inline-example' | 'example';
-  content: string | InlineExample;
+  type: OverviewNodeType;
+  content: string | Example;
 }
+
+export enum OverviewNodeType {
+  TEXT = 'text',
+  LIVE_EXAMPLE = 'live-example',
+  INLINE_EXAMPLE = 'inline-example',
+  STACKED_EXAMPLE = 'stacked-example'
+}
+
